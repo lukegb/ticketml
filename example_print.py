8955021ac99500daec81844f7a5e5828e527aa7e
Luke Granger-Brown <git@lukegb.com>
2015-03-11 23:04:15 +0000

    Adds license header

--- example_print.py
+++ example_print.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python2
+# vim:fileencoding=utf-8
+
+# Copyright (c) 2015 the TicketML authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE.md file.
+
 import argparse
 import ticketml
 import serial
