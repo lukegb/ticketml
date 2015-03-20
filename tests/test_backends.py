7e5a323b466860215ec51929255b346975e4637c
Luke Granger-Brown <git@lukegb.com>
2015-03-20 17:37:38 +0000

    Fixes tests

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -22,7 +22,11 @@ class BackendMixin(object):
         self.mock_serial = mock.MagicMock()
 
     def test_init_sets_left_alignment(self):
-        self.mock_serial.write.assert_any_call(b'\x1ba\x00')  # sets alignment to left
+        self.backend.linebreak()
+        self.mock_serial.write.assert_any_call(b'\n\x1ba\x00')  # sets alignment to left
+
+    def test_init_doesnt_left_align_until_linebreak(self):
+        self.assertFalse(any('\x1ba' in x for ((x,),_) in self.mock_serial.write.call_args_list))
 
     def test_print_text_prints_text(self):
         self.mock_serial.reset_mock()
